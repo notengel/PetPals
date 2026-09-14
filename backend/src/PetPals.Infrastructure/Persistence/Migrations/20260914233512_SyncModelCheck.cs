183feb2015585d049b013d795a8119fcd5fec329
notengel <engito23@gmail.com>
2026-09-14 19:36:50 -0400

    Add migration for syncing model changes and create index on ClinicReviews table
    - Created a new migration file `20260914233512_SyncModelCheck` to sync model changes.
    - Added an index on the `AuthorUserId` column in the `ClinicReviews` table to improve query performance.

---
+++ backend/src/PetPals.Infrastructure/Persistence/Migrations/20260914233512_SyncModelCheck.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetPals.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncModelCheck : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_ClinicReviews_AuthorUserId",
+                table: "ClinicReviews",
+                column: "AuthorUserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ClinicReviews_AuthorUserId",
+                table: "ClinicReviews");
+        }
+    }
+}
